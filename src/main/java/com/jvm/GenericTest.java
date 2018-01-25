9316fc2a2a05963077867799e20ffdaef083e21b
fctest <1379079823@qq.com>
2018-01-25 18:16:26 +0800

    update

--- src/main/java/com/jvm/GenericTest.java
+++ src/main/java/com/jvm/GenericTest.java
@@ -7,18 +7,18 @@ import java.util.ArrayList;
  * @data 2017年3月21日
  * 测试泛型的编译时构造
  */
-public class GenericTest<T extends Base> {
+public class GenericTest<T extends Base<T>> {
 
 	public void test(T clas){
 		System.out.println("hello");
 	}
 	public static void main(String[] args) {
-		new GenericTest<>().test(new Generic());
+		new GenericTest<Generic>().test(new Generic());
 		//泛型只是存在于编译期，运行时会被擦除
 		ArrayList<String> list=new ArrayList<String>();
 		System.out.println(list.size());
 	}
 }
-class Generic extends Base{
+class Generic extends Base<Generic>{
 	
 }
