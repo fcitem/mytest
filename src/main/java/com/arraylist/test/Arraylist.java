821b4d5ae2de7df443516eb1b080f32f5d29c230
fengchao <1379079823@qq.com>
2018-03-30 13:18:19 +0800

    测试分支

--- src/main/java/com/arraylist/test/Arraylist.java
+++ src/main/java/com/arraylist/test/Arraylist.java
@@ -6,67 +6,73 @@ import java.util.List;
 
 public class Arraylist {
 
-	public static void main(String[] args) {
-		String name=new String("herllo");     //变量存储的是引用，而不是引用本身！及存的是引用字符串的引用的地址
-		System.out.println(name.getClass().hashCode());
-		new Arraylist().test(name);
-		System.out.println(name.getClass().hashCode());
-		System.out.println(name);
-		StringBuilder builder=new StringBuilder("sds");
-		new Arraylist().testStringBuilder(builder);
-		System.out.println(builder);
-		Arraylist.testForEach();
-		testIterator();
-		new Arraylist().testFinal();
-	}
-	public int returnfinal(){
-		int number=1;
-		try{
-			return number;
-		}finally {
-			++number;
-		}
-	}
-	public void test(String str){
-		str="fc";
-		System.out.println(str.getClass().hashCode());
-	}
-	public void testStringBuilder(StringBuilder builder){
-		builder.append("sds");
-	}
-	public synchronized final void testFinal() {
-		synchronized (this) {
-			System.out.println("this is final");
-		}
-	}
-	/**
-	 * list在foreach循环中可以移除倒数第二个元素
-	 */
-	public static void testForEach(){
-		List<String> list=new ArrayList<>();
-		list.add("1");
-		list.add("2");
-		list.add("3");
-		for (String string : list) {
-			if("2".equals(string)){
-				list.remove(string);
-			}
-		}
-	}
-	public static void testIterator(){
-		List<String> list=new ArrayList<>();
-		list.add("1");
-		list.add("2");
-		list.add("3");
-		list.add("4");
-		Iterator<String> it=list.iterator();
-		while(it.hasNext()){
-			String str=it.next();
-			System.out.println(str);
-			if(str.equals("2")){
+    public static void main(String[] args) {
+        String name = new String("herllo");     //变量存储的是引用，而不是引用本身！及存的是引用字符串的引用的地址
+        System.out.println(name.getClass().hashCode());
+        new Arraylist().test(name);
+        System.out.println(name.getClass().hashCode());
+        System.out.println(name);
+        StringBuilder builder = new StringBuilder("sds");
+        new Arraylist().testStringBuilder(builder);
+        System.out.println(builder);
+        Arraylist.testForEach();
+        testIterator();
+        new Arraylist().testFinal();
+    }
+
+    public int returnfinal() {
+        int number = 1;
+        try {
+            return number;
+        } finally {
+            ++number;
+        }
+    }
+
+    public void test(String str) {
+        str = "fc";
+        System.out.println(str.getClass().hashCode());
+    }
+
+    public void testStringBuilder(StringBuilder builder) {
+        builder.append("sds");
+    }
+
+    public synchronized final void testFinal() {
+        synchronized (this) {
+            System.out.println("this is final");
+        }
+    }
+
+    /**
+     * list在foreach循环中可以移除倒数第二个元素
+     */
+    public static void testForEach() {
+        List<String> list = new ArrayList<>();
+        list.add("1");
+        list.add("2");
+        list.add("3");
+        for (String string : list) {
+            if ("2".equals(string)) {
+                list.remove(string);
+            }
+        }
+    }
+
+    public static void testIterator() {
+        List<String> list = new ArrayList<>();
+        list.add("1");
+        list.add("2");
+        list.add("3");
+        list.add("4");
+        Iterator<String> it = list.iterator();
+        while (it.hasNext()) {
+            String str = it.next();
+            System.out.println(str);
+            if (str.equals("2")) {
 //				list.remove("2");   //报错
-				it.remove();
-			}
-		}
-	}
+                it.remove();
+            }
+        }
+    }
 }
